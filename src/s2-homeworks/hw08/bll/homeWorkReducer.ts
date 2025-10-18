5beb10be7b504c6f8f6efd9125fad6b91086edbf
nataliaN <asdfghmax@yandex.ru>
2025-10-18 15:03:13 +0300

    hw8-second

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,12 +8,12 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': {
             if(action.payload==='up'){
-                return state.sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()));
+                return [...state].sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()));
             }
-            else return state.sort((a, b) => b.name.toUpperCase().localeCompare(a.name.toUpperCase()));
+            else return [...state].sort((a, b) => b.name.toUpperCase().localeCompare(a.name.toUpperCase()));
         }
         case 'check': {
-            return state.filter(x=>x.age>=18);
+            return state.filter(x=>x.age>=action.payload);
         }
         default:
             return state
