7126f91d5187c857c35e7e7d501f52420af6f262
nataliaN <asdfghmax@yandex.ru>
2025-10-29 15:45:15 +0300

    hw11/1

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -15,7 +15,10 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    const change = (event: any, value: number) => {
+        if(value === value1) {setValue1(event.target.value)}
+        if(value === value2) {setValue2(event.target.value)}
+
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
 
@@ -29,6 +32,8 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+                            value={value1}
+                            onChange={(e)=>change(e,value1)}
                             // сделать так чтоб value1 изменялось // пишет студент
 
                         />
@@ -37,6 +42,8 @@ function HW11() {
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            value={value2}
+                            onChange={(e)=>change(e,value2)}
                             // сделать так чтоб value1/2 изменялось // пишет студент
 
                         />
